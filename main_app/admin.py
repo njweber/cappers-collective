1b248890d3f8f1ea3404b17a23acd209c641185b
Nate Weber <nate.john.weber@gmail.com>
2020-11-09 20:26:30 -0500

    ... now fixed

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import tweets_all
 
-@admin.register(tweet)
+@admin.register(tweets_all)
 class tweet_admin(admin.ModelAdmin):
     list_display = ['id','user','date','text','url']
 
