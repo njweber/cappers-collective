8964c89d41a49082945b6a21bee69e03b48b4971
Nate Weber <nate.john.weber@gmail.com>
2020-11-09 20:31:38 -0500

    fix please

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class tweets_all(models.Model):
     date = models.DateField()
-    user = models.TextField(max_length= 50)
+    name = models.TextField(max_length= 50)
     text = models.TextField()
     url = models.TextField()
     class Meta:
