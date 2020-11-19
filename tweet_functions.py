ac793b55579c261cea75e39bc8efb2b43d8b99ce
Nate Weber <nate.john.weber@gmail.com>
2020-11-18 19:45:16 -0500

    Get tweets from past 24 hour period.

--- tweet_functions.py
+++ tweet_functions.py
@@ -1,18 +1,32 @@
 import tweepy
 import Private
 import DB_Methods
+from datetime import datetime
 
 def start_crawl():
     #Initialize Authentication for Twitter API and Tweepy
     auth = tweepy.OAuthHandler(Private.TWITTER_API_KEY, Private.TWITTER_API_SECRET)
     auth.set_access_token(Private.TWITTER_KEY, Private.TWITTER_SECRET)
     api = tweepy.API(auth)
-
-    #Will need to loop over users here and save tweets that are valid data
-    tweet = api.user_timeline("thecheeze222")[1]
-    date = tweet.created_at
-    name = tweet.user.screen_name  
-    text = tweet.text
-    url = "https://twitter.com/" + name + "/statuses/" + str(tweet.id) 
-    DB_Methods.save_tweet(date, name, text, url)
+    
+    #Loop over users here and save tweets that are valid data
+    users = ["thecheeze222"] #Pull this from DB later
+    for user_num in range(len(users)):
+        flag = False
+        tweet_num = 0
+        while (flag == False):
+            tweet = api.user_timeline(users[user_num])[tweet_num]
+            tweet_date = str(tweet.created_at)[0 : 10]
+            today = str(datetime.today())[0 : 10]
+            if(tweet_date == today): #Tweet from today!
+                tweet_num = tweet_num + 1
+                #TODO: Check to verify if tweet is a bet tweet
+                date = tweet.created_at
+                name = tweet.user.screen_name  
+                text = tweet.text
+                url = "https://twitter.com/" + name + "/statuses/" + str(tweet.id) 
+                DB_Methods.save_tweet(date, name, text, url)
+            else:
+                tweet_num = 0
+                flag = True #Not a tweet from today so go to next user  
     return
