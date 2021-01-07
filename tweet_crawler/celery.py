416e7c33336ee7a2f4f689130041033853487660
Nate Weber <nate.john.weber@gmail.com>
2021-01-07 16:02:32 -0500

    add celery

--- tweet_crawler/celery.py
+++ tweet_crawler/celery.py
@@ -12,10 +12,10 @@ app = Celery('cappers_collective', broker='amqp://localhost')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks('settings.INSTALLED_APPS')
 app.conf.timezone = 'EST'
 
 @app.task(bind=True, name="periodic_task_crawl")
